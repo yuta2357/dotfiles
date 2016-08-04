612f6ff4a5d69240c9476df0ef007c9de2ab7e6b
yuta <hitohasiro2357@outlook.com>
2016-08-04 17:01:35 +0900

    shellscriptの追加

--- dotfilescommit.sh
+++ dotfilescommit.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#自動コミット
+#全てのファイルをコミットするときに使用
+#使い方----- 特定のディレクトリでbash dotfilescommit.shで起動
+#その他
+#一部のコミットやブランチを変える時は手作業で
 
-echo "input comment for commit"
+echo "input comment"
 read comment
 
 git add .
